29a561c80f8f0a69759f709d14e597ddd9503468
Emily Zucker <emilyzucker@ufl.edu>
2026-02-04 17:12:47 -0500

    runnable web

--- ShelfTalk/app/_layout.tsx
+++ ShelfTalk/app/_layout.tsx
@@ -5,7 +5,7 @@ import 'react-native-reanimated';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
 export const unstable_settings = {
-  anchor: '(tabs)',
+  initialRouteName: 'login',
 };
 
 export default function RootLayout() {
@@ -13,11 +13,14 @@ export default function RootLayout() {
 
   return (
     <>
-      <Stack>
+      <Stack screenOptions={{ animationEnabled: false }}>
+        <Stack.Screen name="login" options={{ headerShown: false }} />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="modal" options={{ presentation: 'modal', title: 'Modal' }} />
       </Stack>
       <StatusBar style="auto" />
     </>
   );
+
+
 }
