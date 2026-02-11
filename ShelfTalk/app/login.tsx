2d356e53af78e2a0fe1498be79b2afd79406758e
isa-par06 <isapareja06@gmail.com>
2026-02-11 11:38:32 -0500

    firebase connected to auth code

--- ShelfTalk/app/login.tsx
+++ ShelfTalk/app/login.tsx
@@ -4,20 +4,32 @@ import { StyleSheet, TextInput, Button, Alert } from 'react-native';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
 import { useRouter } from 'expo-router';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from './firebase';
 
 export default function LoginScreen() {
   const router = useRouter();
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleLogin = () => {
-    // Simple validation
-    if (email === 'test@example.com' && password === 'password') {
-      // Fake login success
+  const handleLogin = async () => {
+    //checks with firebase to ensure the email + password belongs to a valid user
+    //if not, then an error is thrown and the user is alerted that the login failed
+    try {
+      const cred = await signInWithEmailAndPassword(auth, email, password);
+      console.log("Firebase user:", cred.user.uid);
       router.replace('/(tabs)/explore');
-    } else {
-      Alert.alert('Error', 'Invalid credentials');
+    } catch (error) {
+      console.log(error);
+      Alert.alert("Login failed");
     }
+    // // Simple validation
+    // if (email === 'test@example.com' && password === 'password') {
+    //   // Fake login success
+    //   router.replace('/(tabs)/explore');
+    // } else {
+    //   Alert.alert('Error', 'Invalid credentials');
+    // }
   };
 
   return (
